5021a6136ea8fcb4d0134239b61d678205714a51
mateushlsilva <mateushls01@gmail.com>
2025-11-16 10:39:08 -0300

    feat(health): Configura o setup base para health checks do api-gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.enableShutdownHooks();
 
   const config = new DocumentBuilder()
     .setTitle('Task Management API')
