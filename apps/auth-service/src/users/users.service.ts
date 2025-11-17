de1093518ddd215b31b53edc699b6ad41aa5d4c3
mateushlsilva <mateushls01@gmail.com>
2025-11-17 16:19:56 -0300

    feat(users): Implementar UsersService e lógica de validação de credenciais de login

--- apps/auth-service/src/users/users.service.ts
+++ apps/auth-service/src/users/users.service.ts
@@ -1,16 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user';
 import { Repository } from 'typeorm';
+import { CreateUserDto, LoginUserDTO } from '@app/common';
+import { PinoLogger } from 'nestjs-pino';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private repository: Repository<User>,
-  ) {}
+    private readonly logger: PinoLogger,
+    private jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {
+    this.logger.setContext(UsersService.name);
+  }
 
   async findAll(): Promise<User[]> {
     return this.repository.find();
+  }
+
+  async login(data: LoginUserDTO) {
+    const userEmail = data.userEmail;
+    const find = await this.repository
+      .createQueryBuilder('user')
+      .select()
+      .addSelect('user.userPassword')
+      .where('user.userEmail=:userEmail', { userEmail })
+      .getOne();
+
+    if (!find) {
+      throw new UnauthorizedException();
+    }
+
+    const isPass = await find.compare(data.userPassword);
+
+    if (!isPass) {
+      throw new UnauthorizedException();
+    }
+
+    return {
+      id: find.id,
+      userEmail: find.userEmail,
+    };
+  }
+
+  async create(data: CreateUserDto) {
+    const newUser = this.repository.create({
+      userEmail: data.userEmail,
+      userName: data.userName,
+      userPassword: '',
+    });
+
+    const save = await this.repository.save(newUser);
+
+    return { save };
   }
 }
