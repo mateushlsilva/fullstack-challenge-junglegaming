70f65a37faddcdfdf010a6f3bce625124ab4988c
mateushlsilva <mateushls01@gmail.com>
2025-11-17 16:14:39 -0300

    feat(auth): Adicionar DTOs de CreateUser e Login para validação

---
+++ packages/common/src/dtos/user/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+    @IsEmail({}, { message: 'O e-mail deve ser um endereço válido.' })
+    @IsNotEmpty({ message: 'O campo e-mail é obrigatório.' })
+    @IsString()
+    userEmail: string;
+
+    @IsString()
+    @MaxLength(50)
+    userName: string;
+
+    @IsNotEmpty({ message: 'O campo senha é obrigatório.' })
+    @IsString()
+    @MinLength(8, { message: 'A senha deve ter pelo menos 8 caracteres.' })
+    @MaxLength(128)
+    userPassword: string
+}
---
+++ packages/common/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./dtos/user/create-user.dto";
+export * from "./dtos/user/login-user.dto";
+export *  from "./interface/jwt-payload.interface";
+export * from "./guard/auth.guard";
+export * from './filters/all-exceptions.filter';
