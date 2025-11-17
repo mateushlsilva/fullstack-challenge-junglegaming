61cac526e0ee5cf51a07fc1076d23fc9b09e49a4
mateushlsilva <mateushls01@gmail.com>
2025-11-17 19:36:57 -0300

    feat(gateway): Adicionar endpoint POST /auth/register

--- apps/api-gateway/src/auth/auth.controller.ts
+++ apps/api-gateway/src/auth/auth.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from '@app/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +20,20 @@ export class AuthController {
     // Chama o método find() no AuthService, que envia o comando { cmd: 'find_user' } para o RMQ.
     // O Gateway espera a resposta e a retorna como JSON HTTP.
     return this.authService.find();
+  }
+
+  @Post('/register')
+  @ApiOperation({ summary: 'Registrar um novo usuário' })
+  @ApiResponse({ status: 201, description: 'Usuário registrado com sucesso.' })
+  @ApiResponse({
+    status: 400,
+    description: 'Dados de entrada inválidos (validação DTO).',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Email já está sendo usado.',
+  })
+  async register(@Body() data: CreateUserDto) {
+    return await this.authService.register(data);
   }
 }
--- apps/api-gateway/src/auth/auth.service.ts
+++ apps/api-gateway/src/auth/auth.service.ts
@@ -1,9 +1,22 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { AUTH_SERVICE } from './auth.constants';
 import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserDto } from '@app/common';
+import { lastValueFrom } from 'rxjs';
+
+interface Error {
+  code: number;
+  message: string;
+}
 
 @Injectable()
-export class AuthService {
+export class AuthService implements OnModuleInit {
   constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
 
   async onModuleInit() {
@@ -12,5 +25,25 @@ export class AuthService {
 
   async find() {
     return this.authClient.send({ cmd: 'find_user' }, {});
+  }
+
+  async register(data: CreateUserDto) {
+    try {
+      const res = await lastValueFrom(
+        this.authClient.send<CreateUserDto>({ cmd: 'create_user' }, data),
+      );
+
+      return res;
+    } catch (error: unknown) {
+      const err: Error = error as Error;
+      console.error(err);
+      if (err.code === 409) {
+        throw new ConflictException(err.message);
+      }
+
+      throw new InternalServerErrorException(
+        err?.message || 'Erro inesperado no gateway',
+      );
+    }
   }
 }
