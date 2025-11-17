5ad9b281b16fbec5cd6917f0e536df01c8e65439
mateushlsilva <mateushls01@gmail.com>
2025-11-17 16:16:14 -0300

    feat(security): Implementar AuthGuard e definir interface JWTPayload

---
+++ packages/common/src/guard/auth.guard.ts
@@ -0,0 +1,49 @@
+
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    private JWT_ACCESS_SECRET: string;
+  constructor(
+        private jwtService: JwtService,
+        private readonly configService: ConfigService,
+    ) {
+        this.JWT_ACCESS_SECRET = this.configService.get<string>(
+      'JWT_ACCESS_SECRET',
+    ) as string;
+    }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    console.log('Context type:', context.getType());
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        {
+          secret: this.JWT_ACCESS_SECRET
+        }
+      );
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
