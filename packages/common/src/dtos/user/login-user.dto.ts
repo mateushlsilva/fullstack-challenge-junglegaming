19f2cde9cc4a892b35819f3094eb6fc731f693ea
mateushlsilva <mateushls01@gmail.com>
2025-11-18 09:23:49 -0300

    docs(swagger): Adicionar decoradores @ApiProperty ao LoginUserDto

--- packages/common/src/dtos/user/login-user.dto.ts
+++ packages/common/src/dtos/user/login-user.dto.ts
@@ -1,11 +1,22 @@
 import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'
 
 export class LoginUserDTO {
+    @ApiProperty({
+        description: 'O endereço de email único do usuário (será usado para login).',
+        example: 'joao.silva@exemplo.com',
+    })
     @IsEmail({}, { message: 'O e-mail deve ser um endereço válido.' })
     @IsNotEmpty({ message: 'O campo e-mail é obrigatório.' })
     @IsString()
     userEmail: string;
 
+
+    @ApiProperty({
+        description: 'A senha do usuário.',
+        example: 'senhaSegura123',
+        minLength: 8,
+    })
     @IsNotEmpty({ message: 'O campo senha é obrigatório.' })
     @IsString()
     @MinLength(8, { message: 'A senha deve ter pelo menos 8 caracteres.' })
