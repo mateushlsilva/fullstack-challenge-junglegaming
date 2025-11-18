42b2dfc9ad7e58972d6084d69c14f30e4949cf39
mateushlsilva <mateushls01@gmail.com>
2025-11-18 15:45:25 -0300

    feat(tasks): Criar TasksModule, Controller e Service no task-service

---
+++ apps/tasks-service/src/task/task.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { TaskController } from './task.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { TaskAssignee } from './entities/task-assignee.entity';
+import { TaskHistory } from './entities/task-history.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Task, TaskAssignee, TaskHistory])],
+  providers: [TaskService],
+  controllers: [TaskController],
+  exports: [TypeOrmModule],
+})
+export class TaskModule {}
