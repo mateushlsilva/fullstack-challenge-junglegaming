850bac3388a8c64de4150c5981d38b02b13ea842
mateushlsilva <mateushls01@gmail.com>
2025-11-18 18:35:05 -0300

    feat(tasks): Implementar a lógica de criação de tarefa no TasksService

--- apps/tasks-service/src/task/task.controller.ts
+++ apps/tasks-service/src/task/task.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { PinoLogger } from 'nestjs-pino';
+import { CreateTaskDto } from '@app/common';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(
+    private taskService: TaskService,
+    private readonly logger: PinoLogger,
+  ) {
+    this.logger.setContext(TaskController.name);
+  }
+
+  @Post()
+  async post(@Body() data: CreateTaskDto) {
+    return await this.taskService.createTask(data, 1);
+  }
+}
--- apps/tasks-service/src/task/task.service.ts
+++ apps/tasks-service/src/task/task.service.ts
@@ -1,4 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PinoLogger } from 'nestjs-pino';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
+import { TaskAssignee } from './entities/task-assignee.entity';
+import { TaskHistory } from './entities/task-history.entity';
+import { ActionEnum, CreateTaskDto } from '@app/common';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: Repository<Task>,
+
+    @InjectRepository(TaskAssignee)
+    private taskAssigneeRepository: Repository<TaskAssignee>,
+
+    @InjectRepository(TaskHistory)
+    private taskHistoryRepository: Repository<TaskHistory>,
+    private readonly logger: PinoLogger,
+  ) {
+    this.logger.setContext(TaskService.name);
+  }
+
+  // GET, POST, PUT, DELETE, GETpage,size
+
+  async createTask(data: CreateTaskDto, creator_id: number) {
+    return await this.taskRepository.manager.transaction(async (manager) => {
+      const newTask = manager.create(Task, {
+        taskTitle: data.taskTitle,
+        taskDescription: data.taskDescription,
+        taskDueDate: new Date(data.taskDueDate),
+        taskPriority: data.taskPriority,
+        taskStatus: data.taskStatus,
+      });
+
+      const savedTask = await manager.save(newTask);
+
+      // Associa o creator
+      const creatorAssignee = manager.create(TaskAssignee, {
+        task: savedTask,
+        user_id: creator_id,
+      });
+      await manager.save(creatorAssignee);
+
+      // Associa os outros usuários (exceto o creator)
+      const otherUserIds =
+        data.assigned_user_ids?.filter((id) => id !== creator_id) || [];
+      if (otherUserIds.length) {
+        const assignees = otherUserIds.map((userId) =>
+          manager.create(TaskAssignee, {
+            task: savedTask,
+            user_id: userId,
+          }),
+        );
+        await manager.save(assignees);
+      }
+
+      const history = manager.create(TaskHistory, {
+        task: savedTask,
+        user_id: creator_id,
+        new_value: savedTask,
+        action: ActionEnum.CREATED,
+      });
+      await manager.save(history);
+
+      return savedTask;
+    });
+  }
+}
