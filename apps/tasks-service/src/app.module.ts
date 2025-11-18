bdd85feab96842f4d638b15ff3c8cf80caf31a74
mateushlsilva <mateushls01@gmail.com>
2025-11-18 15:43:46 -0300

    feat(orm): Configurar e estabelecer conexão com TypeORM no task-service

--- apps/tasks-service/src/app.module.ts
+++ apps/tasks-service/src/app.module.ts
@@ -3,6 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
 import { HealthModule } from './health/health.module';
+import { TaskModule } from './task/task.module';
+import { CommentsModule } from './comments/comments.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -18,7 +22,24 @@ import { HealthModule } from './health/health.module';
         },
       },
     }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     HealthModule,
+    TaskModule,
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
