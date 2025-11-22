ccc708acddeb6cf391c45ff021e7e758e4ce5ca7
mateushlsilva <mateushls01@gmail.com>
2025-11-22 08:51:12 -0300

    feat(notifications): Implementa recuperação de notificação e atualização para 'READ'

--- apps/api-gateway/src/notification/notification.service.ts
+++ apps/api-gateway/src/notification/notification.service.ts
@@ -1,33 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { WebSocketNotification } from './websocket';
 import { PinoLogger } from 'nestjs-pino';
 import {
   CommentCreatedEventDto,
+  CreateNotificationDto,
   TaskCreatedEventDto,
   TaskUpdatedEventDto,
 } from '@app/common';
+import { NOTIFICATION_GATEWAY_SERVICE } from './notification-gateway.constants';
+import { ClientProxy } from '@nestjs/microservices';
 
+interface Payload {
+  message: string;
+  data: TaskCreatedEventDto | TaskUpdatedEventDto | CommentCreatedEventDto;
+}
 @Injectable()
 export class NotificationService {
   constructor(
+    @Inject(forwardRef(() => WebSocketNotification))
     private readonly gateway: WebSocketNotification,
+    @Inject(NOTIFICATION_GATEWAY_SERVICE)
+    private readonly notificationGatewayClient: ClientProxy,
     private readonly logger: PinoLogger,
   ) {
     this.logger.setContext(WebSocket.name);
   }
 
-  async sendToUser(data: {
+  sendToUser(data: {
+    id: string;
     userId: string;
     event: string;
-    payload: {
-      message: string;
-      data: TaskCreatedEventDto | TaskUpdatedEventDto | CommentCreatedEventDto;
-    };
+    payload: Payload;
   }) {
     this.logger.info(`Enviando WS para user ${data.userId}`);
-
-    this.gateway.server.to(data.userId).emit(data.event, data.payload);
+    this.gateway.server
+      .to(data.userId)
+      .emit(data.event, { id: data.id, ...data.payload });
   }
 
-  async find() {}
+  findStatusUnread(userId: string) {
+    this.logger.debug(`Entrou aqui e recebeu o id desse usuario: ${userId}`);
+    this.notificationGatewayClient
+      .send<CreateNotificationDto[]>({ cmd: 'notify_find' }, userId)
+      .subscribe((notifications) => {
+        for (const notification of notifications) {
+          if (!notification.id) {
+            this.logger.warn('Notificação sem ID');
+            continue;
+          }
+          this.sendToUser({
+            id: notification.id,
+            event: notification.type,
+            userId,
+            payload: notification.payload as Payload,
+          });
+        }
+      });
+    this.logger.debug(`Já pegou tudo do id desse usuario aqui: ${userId}`);
+  }
+
+  patchStatus(id: string) {
+    this.notificationGatewayClient.emit('notify.status', id);
+  }
 }
--- apps/api-gateway/src/notification/websocket.ts
+++ apps/api-gateway/src/notification/websocket.ts
@@ -1,11 +1,15 @@
 import { Server, Socket } from 'socket.io';
 import {
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { PinoLogger } from 'nestjs-pino';
+import { NotificationService } from './notification.service';
+import { forwardRef, Inject } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
@@ -17,33 +21,34 @@ export class WebSocketNotification
 {
   @WebSocketServer()
   server: Server;
-  constructor(private readonly logger: PinoLogger) {
+  constructor(
+    private readonly logger: PinoLogger,
+    @Inject(forwardRef(() => NotificationService))
+    private readonly notificationService: NotificationService,
+  ) {
     this.logger.setContext(WebSocket.name);
   }
 
   handleConnection(client: Socket) {
-    // 1. Extrai o header. O tipo é: string | string[] | undefined
+    // Extrai o header. O tipo é: string | string[] | undefined
     const rawUserId = client.handshake.headers['x-user-id'];
 
     let userId: string | undefined;
 
-    // 2. Normaliza o valor: se for array, pega o primeiro; se for string, mantém; se for undefined, mantém.
+    // Normaliza o valor: se for array, pega o primeiro; se for string, mantém; se for undefined, mantém.
     if (Array.isArray(rawUserId)) {
       userId = rawUserId[0];
     } else {
       userId = rawUserId;
     }
 
-    // 3. Type Guard: Garante que userId existe antes de tentar fazer o join.
     if (userId) {
-      // client.join(userId) agora é seguro, pois userId é definitivamente uma string.
       client.join(userId);
       this.logger.info(
         `Usuário ${userId} conectado (Socket ID: ${client.id}).`,
       );
+      this.notificationService.findStatusUnread(userId);
     } else {
-      // Se não houver ID de usuário, loga um aviso e não tenta o join,
-      // prevenindo o erro de compilação do TypeScript.
       this.logger.warn(
         `Client sem 'x-user-id' header. Conexão limitada: ${client.id}.`,
       );
@@ -51,5 +56,10 @@ export class WebSocketNotification
   }
   handleDisconnect(client: Socket) {
     this.logger.info(`Client desconectado: ${client.id}`);
+  }
+
+  @SubscribeMessage('notification:received')
+  handleAck(@MessageBody() id: string) {
+    this.notificationService.patchStatus(id);
   }
 }
