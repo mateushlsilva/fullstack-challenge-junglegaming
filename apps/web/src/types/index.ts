52d0f4d33d23172ce95096c081886c7fa141efbe
mateushlsilva <mateushls01@gmail.com>
2025-11-23 14:26:02 -0300

    feat(types): Define o tipo UpdateTaskType

---
+++ apps/web/src/types/index.ts
@@ -0,0 +1,9 @@
+import type { CreateTaskType } from "./tasks/create-task.type";
+import type { GetQueryType } from './tasks/get-query.type';
+import type { UpdateTaskType } from "./tasks/update-task.type";
+
+export type {
+    CreateTaskType,
+    GetQueryType,
+    UpdateTaskType
+}
---
+++ apps/web/src/types/tasks/update-task.type.ts
@@ -0,0 +1,19 @@
+import { PriorityEnum, StatusEnum } from '@app/common';
+
+type Assignees = {
+    id: number,
+    user_id: string,
+    assigned_at: Date,
+}
+
+export type UpdateTaskType = {
+    id: number,
+    taskTitle: string,
+    taskDescription: string,
+    taskDueDate: Date,
+    taskPriority: PriorityEnum,
+    taskStatus: StatusEnum,
+    created_at: Date,
+    updated_at: Date,
+    assignees: Assignees[]
+}
