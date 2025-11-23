6881519b9714c5cbe9ee4ee4c9c57cd70aadcf4b
mateushlsilva <mateushls01@gmail.com>
2025-11-23 14:24:55 -0300

    feat(types): Define o tipo GetQueryType para requisições de paginação

---
+++ apps/web/src/types/tasks/get-query.type.ts
@@ -0,0 +1,34 @@
+import { PriorityEnum, StatusEnum } from '@app/common';
+
+type Assignees = {
+    id: number,
+    user_id: string,
+    assigned_at: Date,
+}
+
+type Comments = {
+    id: number,
+    user_id: string,
+    content: string,
+    created_at: Date
+}
+
+type GetTaskAssigneesAndCommentsType = {
+    id: number,
+    taskTitle: string,
+    taskDescription: string,
+    taskDueDate: Date,
+    taskPriority: PriorityEnum,
+    taskStatus: StatusEnum,
+    created_at: Date,
+    updated_at: Date,
+    assignees: Assignees[],
+    comments: Comments[],
+}
+
+export type GetQueryType = {
+    page: number,
+    size: number,
+    total: number,
+    data: GetTaskAssigneesAndCommentsType[],
+}
