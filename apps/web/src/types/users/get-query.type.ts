a4daf86933b4d53a29794873243081dbd22cb080
mateushlsilva <mateushls01@gmail.com>
2025-11-24 09:47:31 -0300

    feat(types): Define o tipo GetQueryUsersType

---
+++ apps/web/src/types/users/get-query.type.ts
@@ -0,0 +1,12 @@
+type UserTypeQuery = {
+    id: number,
+    userEmail: string,
+    userName: string,
+};
+
+export type GetQueryUsersType = {
+    page: number,
+    size: number,
+    total: number,
+    data: UserTypeQuery[]
+};
