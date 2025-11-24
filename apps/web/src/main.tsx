b78f809a88d35427335fef75b703fcbdd7774056
mateushlsilva <mateushls01@gmail.com>
2025-11-24 18:50:31 -0300

    feat(router): Configuração inicial do TanStack Router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,19 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import "./api/interceptor.ts";
-import App from './App.tsx'
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import './index.css'
+import "./api/interceptor.ts";
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen.ts';
 
+
+const router = createRouter({ routeTree })
 const queryClient = new QueryClient()
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
 )
