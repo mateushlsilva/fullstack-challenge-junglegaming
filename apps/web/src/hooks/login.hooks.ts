0e1619bc19251de3824e42167c200a52ad9decef
mateushlsilva <mateushls01@gmail.com>
2025-11-25 14:35:27 -0300

    feat(auth): Melhora useLogin com navegação para Home e Toast de erro

--- apps/web/src/hooks/login.hooks.ts
+++ apps/web/src/hooks/login.hooks.ts
@@ -2,19 +2,25 @@ import { useMutation } from "@tanstack/react-query";
 import { AuthService } from "../services";
 import { useAuthStore } from "../stores";
 import type { LoginDto } from "../schemas";
+import { useNavigate } from "@tanstack/react-router";
+import { toast } from "sonner"
+
 
 export const useLogin = () => {
   const setAuth = useAuthStore((state) => state.setAuth);
+  const navigate = useNavigate();
 
   return useMutation({
     mutationFn: (body: LoginDto) => AuthService.login(body),
 
     onSuccess: (data, { userEmail }) => {
       setAuth(data.access_token, { userEmail });
+      navigate({ to: '/home' })
     },
 
     onError: (err) => {
       console.error("Erro no login:", err);
+      toast.error('Erro ao fazer login, dados inválidos!')
     },
   });
 };
