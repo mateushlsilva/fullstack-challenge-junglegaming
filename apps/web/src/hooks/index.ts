d042ba877054f26c826f30ff0c7ca2cf9e06282f
mateushlsilva <mateushls01@gmail.com>
2025-11-27 13:29:14 -0300

    feat(comments): Cria useCommentCreate (Mutation) para adicionar novos comentários

--- apps/web/src/hooks/index.ts
+++ apps/web/src/hooks/index.ts
@@ -4,6 +4,9 @@ import { useTaskQuery } from "./queries/tasks-getquery.hook";
 import { useUserQuery } from "./queries/users-getquery.hook";
 import { useTaskCreate } from "./mutations/task-create.hooks";
 import { useAuthWebSocket } from "./websocket/web-socket.hook";
+import { useCommentCreate } from "./mutations/commests-create.hook";
+import { useTaskUpdate } from "./mutations/task-update.hooks";
+import { useKanbanStatus } from "./mutations/kanban-status.hooks";
 
 export {
     useLogin,
@@ -12,4 +15,7 @@ export {
     useUserQuery,
     useTaskCreate,
     useAuthWebSocket,
+    useCommentCreate,
+    useTaskUpdate,
+    useKanbanStatus,
 }
---
+++ apps/web/src/hooks/mutations/commests-create.hook.ts
@@ -0,0 +1,29 @@
+import { useMutation } from "@tanstack/react-query";
+import { CommentsService } from "../../services";
+import type { CreateCommentDto } from "../../schemas";
+
+import { toast } from "sonner"
+
+type CommentCreateProps = {
+    id: number;
+    body: CreateCommentDto;
+}
+
+export const useCommentCreate = () => {
+
+  return useMutation({
+    mutationFn: ({ id ,body }: CommentCreateProps) => CommentsService.create(id, body),
+
+    onSuccess: () => {
+      toast.success('Comentário criado!')
+    },
+
+    onError: (err) => {
+      let message = "Erro desconhecido"
+      if (err?.response?.data?.statusCode === 400) message = "Dados Inválidos"
+     
+      console.error("Erro cadastro do Comentário:", message);
+      toast.error(`Erro ao criar o comentário. ${message}!`)
+    },
+  });
+};
