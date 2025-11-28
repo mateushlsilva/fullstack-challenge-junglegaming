c487b0d4bc49eeae7fbf19229216885752b4357e
mateushlsilva <mateushls01@gmail.com>
2025-11-28 12:07:58 -0300

    feat(tasks): Cria useTaskUpdate (Mutation) para edição de detalhes da tarefa

--- apps/web/src/hooks/mutations/task-update.hooks.ts
+++ apps/web/src/hooks/mutations/task-update.hooks.ts
@@ -3,16 +3,22 @@ import { TaskService } from "../../services";
 import type { CreateTaskDto } from "../../schemas";
 
 import { toast } from "sonner"
+import { useNavigate } from "@tanstack/react-router";
 
+type TaskUpdate = {
+  id: number;
+  body: CreateTaskDto
+}
 
+export const useTaskUpdate = () => {
 
-export const useTaskUpdate = (id: number) => {
-
+  const navigate = useNavigate();
   return useMutation({
-    mutationFn: (body: CreateTaskDto) => TaskService.update(id, body),
+    mutationFn: ({ id, body }: TaskUpdate) => TaskService.update(id, body),
 
     onSuccess: () => {
       toast.success('Task atualiza!')
+      navigate({ to: '/' })
     },
 
     onError: (err) => {
